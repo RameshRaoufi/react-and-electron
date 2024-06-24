e418f1cbb7775d7c25323c3525dc040236609539
ramesh-raoufi <rameshraoufi1@gmail.com>
2024-06-24 15:51:36 +0430

    react project integrated with electronJs

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname, 'build/index.html')}`;
+  mainWindow.loadURL(startUrl);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
